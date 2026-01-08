cc6441334c9c49c96114de83911831111dac1297
WhiteLatexs <168267152+WhiteLatexs@users.noreply.github.com>
2026-01-08 21:04:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   ownerId: '590354442214178856',
   status: {
     rotateDefault: [
-      { name: 'Netflix', type: ActivityType.Watching },
+      { name: 'Yoel Yapping', type: ActivityType.Watching },
       { name: 'Minecraft', type: ActivityType.Playing },
       { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.youtube.com/Hayogo' },
       { name: 'Spotify', type: ActivityType.Custom },
@@ -14,4 +14,5 @@ module.exports = {
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
 }
+
 
