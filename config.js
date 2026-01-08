83e17693daa0869a20866e2a934d29b19abd6350
WhiteLatexs <168267152+WhiteLatexs@users.noreply.github.com>
2026-01-08 21:04:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '590354442214178856',
   status: {
     rotateDefault: [
       { name: 'Netflix', type: ActivityType.Watching },
-      { name: 'GTA VI', type: ActivityType.Playing },
-      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
+      { name: 'Minecraft', type: ActivityType.Playing },
+      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.youtube.com/Hayogo' },
       { name: 'Spotify', type: ActivityType.Custom },
     ],
     songStatus: true
@@ -14,3 +14,4 @@ module.exports = {
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
 }
+
